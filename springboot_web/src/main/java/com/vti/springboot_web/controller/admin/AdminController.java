fd095ccb19fbc420897d879ba8418bfd4a966fb9
hoangphuc <nguyenhoangphuc020803@gmail.com>
2024-12-29 18:09:39 +0700

    upload

---
+++ springboot_web/src/main/java/com/vti/springboot_web/controller/admin/AdminController.java
@@ -0,0 +1,12 @@
+package com.vti.springboot_web.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin/index";
+    }
+}
